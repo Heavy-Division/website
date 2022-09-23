7cdc3186aceee8a9203f49b7046dfb25efac4b61
Hypeism <jhnwllms236@gmail.com>
2022-09-23 20:38:53 +0900

    emergency downloads link page fix

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -5,11 +5,14 @@ import { Button } from '../components/utils';
 type downloadItemProps = {
     children: ReactNode,
     description: string
+    href: string,
 }
 
 export const DownloadItem = (props: downloadItemProps) => (
     <div className="my-4 overflow-hidden px-4 text-white sm:my-3 sm:px-3 md:my-3 md:w-1/3 md:px-3 lg:my-1 lg:w-1/3 lg:px-1 xl:my-6 xl:w-1/3 xl:px-6">
-        <Button className="mt-12 h-16 w-48 border-2 border-white text-xl hover:bg-white hover:text-navy sm:text-sm">{props.children}</Button>
+        <Button className="mt-12 h-16 w-48 border-2 border-white text-xl hover:bg-white hover:text-navy sm:text-sm">
+            <a href={props.href}>{props.children}</a>
+        </Button>
         <hr className="mt-10" />
         <p className="mt-12 w-48 text-center text-xl sm:text-start sm:text-sm">{props.description}</p>
     </div>
@@ -18,6 +21,10 @@ export const DownloadItem = (props: downloadItemProps) => (
 const stableSummary = 'The Stable Build is our most tested and reliable version, with features following strict requirements to ensure a smooth experience. ';
 const developmentSummary = 'The Development Build includes new features ready for public preview, but may encounter some bugs.';
 const experimentalSummary = 'The Experimental Build includes brand new features for public testing. No support will be provided for this build.';
+
+const stableDownloadUrl = "https://github.com/Heavy-Division/B78XH/releases/download/v0.1.14/B78XH-v0.1.14.zip";
+const devDownloadUrl = "https://github.com/Heavy-Division/B78XH/archive/refs/heads/main.zip";
+const experimentalDownloadUrl = "https://github.com/Heavy-Division/B78XH/archive/refs/heads/experimental.zip";
 
 const Downloads = () => (
     <div className="px-6 py-16">
@@ -29,9 +36,9 @@ const Downloads = () => (
         </div>
         <div className="flex justify-center">
             <div className="flex flex-col gap-x-5 sm:flex-row">
-                <DownloadItem description={stableSummary}>Stable</DownloadItem>
-                <DownloadItem description={developmentSummary}>Development</DownloadItem>
-                <DownloadItem description={experimentalSummary}>Experimental</DownloadItem>
+                <DownloadItem href={stableDownloadUrl} description={stableSummary}>Stable</DownloadItem>
+                <DownloadItem href={devDownloadUrl} description={developmentSummary}>Development</DownloadItem>
+                <DownloadItem href={experimentalDownloadUrl} description={experimentalSummary}>Experimental</DownloadItem>
             </div>
         </div>
     </div>
