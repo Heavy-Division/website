726762b40e9a13d721b2c8a9c552a3871ed2ac5b
nakajimayoshi <Yoshi@hdsimulations.com>
2023-07-24 08:22:59 +0900

    remove stable and development links

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -9,38 +9,32 @@ type downloadItemProps = {
 }
 
 export const DownloadItem = (props: downloadItemProps) => (
-    <div className="my-4 overflow-hidden px-4 text-white sm:my-3 sm:px-3 md:my-3 md:w-1/3 md:px-3 lg:my-1 lg:w-1/3 lg:px-1 xl:my-6 xl:w-1/3 xl:px-6">
+    <div className="my-4 overflow-hidden px-4 text-white">
         <a href={props.href}>
-            <Button className="mt-12 h-16 w-48 border-2 border-white text-xl hover:bg-white hover:text-navy sm:text-sm">
+            <Button className="mt-12 h-16 w-48 border-2 border-white text-xl hover:bg-white hover:text-navy">
                 {props.children}
             </Button>
         </a>
         <hr className="mt-10" />
-        <p className="mt-12 w-48 text-center text-xl sm:text-start sm:text-sm">{props.description}</p>
+        <p className="mt-12 w-48 text-center text-xl sm:text-start">{props.description}</p>
     </div>
 );
 
-const stableSummary = 'The Stable Build is our most tested and reliable version, with features following strict requirements to ensure a smooth experience. ';
-const developmentSummary = 'The Development Build includes new features ready for public preview, but may encounter some bugs.';
-const experimentalSummary = 'The Experimental Build includes brand new features for public testing. No support will be provided for this build.';
-
-const stableDownloadUrl = 'https://github.com/Heavy-Division/B78XH/releases/download/v0.1.15/B78XH-v0.1.15.zip';
-const devDownloadUrl = 'https://github.com/Heavy-Division/B78XH/archive/refs/heads/main.zip';
-const experimentalDownloadUrl = 'https://github.com/Heavy-Division/B78XH/archive/refs/heads/experimental.zip';
+const downloadURL = 'https://github.com/Heavy-Division/B78XH/archive/refs/heads/experimental.zip';
 
 const Downloads = () => (
     <Container className="mx-auto mt-48">
         <div className="flex h-[50vh] justify-center">
-            <Image src="/svg/78x-side.svg" alt="787-10_side" width={1000} height={330} draggable={false} className="absolute mx-4 object-contain" />
+            <Image src="/svg/78x-side.svg" alt="787-10_side" width={1000} height={330} draggable={false}
+                   className="absolute mx-4 object-contain" />
         </div>
         <div className="flex justify-center">
             <Image src="/svg/B78XH.svg" alt="B78XH" height={68} width={350} draggable={false} />
         </div>
         <div className="flex justify-center">
             <div className="mb-10 flex flex-col gap-x-5 sm:flex-row">
-                <DownloadItem href={stableDownloadUrl} description={stableSummary}>Stable</DownloadItem>
-                <DownloadItem href={devDownloadUrl} description={developmentSummary}>Development</DownloadItem>
-                <DownloadItem href={experimentalDownloadUrl} description={experimentalSummary}>Experimental</DownloadItem>
+                <DownloadItem href={downloadURL}
+                              description={'Available only for the premium deluxe version of Microsoft Flight Simulator 2020'}>v1.2.0</DownloadItem>
             </div>
         </div>
     </Container>
